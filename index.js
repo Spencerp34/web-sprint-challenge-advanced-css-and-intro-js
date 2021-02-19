add12765d91294a8f58e49af15dcf4a189e675aa
Spencerp34 <spencerp34@yahoo.com>
2021-02-19 10:07:10 -0700

    did remaining tasks for MVP

--- index.js
+++ index.js
@@ -247,8 +247,18 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  const filteredArray = [];
+  for(let i =0; i < array.length; i++){
+    const splitYears = array[i].years.split(' - ');
+    const birthYear = Number(splitYears[0]);
+    const deathYear = Number(splitYears[1]);
+    if(birthYear >= 1900 && deathYear <= 2000){
+      filteredArray.push(array[i].name)
+    }
+  
+  }
+  return filteredArray;
 }
 
 
@@ -262,8 +272,9 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+ function removeArtist(array, index){
+  array.splice(index, 1)
+  return array.length
 }
    
 
@@ -283,11 +294,17 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(array){
+  array.push({ 
+      id: '20',
+      name: 'Spencer Patterson', 
+      years: '1998 - 2021',
+      genre: 'Web Design', 
+      nationality: 'American',
+      bio: 'I am young, learning Web Developemen, and hoping to pass my Sprint today! I hope to take my new found programming skills and learn game developement next!'  
+    });
+    return array
   }
-
-  
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use lotsOfArt to do the following: 
@@ -296,8 +313,14 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(array){
+  const filteredArray = [];
+  for(let i = 0; i < array.length; i++){
+    if(array[i].paintings > 100){
+      filteredArray.push(array[i].name)
+    }
+  }
+  return filteredArray;
 }
 
 
